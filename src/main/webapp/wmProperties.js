7a7533bfbfcf6f2d224f13444c2d7a14bf81fedb
root <root@d3a2e0704417>
2017-06-02 00:29:11 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_dpNdCBJdzO",
+  "homePage" : "Main",
+  "name" : "Auto_dpNdCBJdzO",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
